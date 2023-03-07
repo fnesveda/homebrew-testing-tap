0744e1714a6d7ecd57947954ad7bc656087d6430
František Nesveda <frantisek.nesveda@gmail.com>
2023-03-07 10:15:23 +0100

    Debug

--- Formula/apify-cli.rb
+++ Formula/apify-cli.rb
@@ -7,10 +7,12 @@ class ApifyCli < Formula
   sha256 "f4f0d95d77559a07ebb2e7d1b36f7ce7b837fd808a1f8a086e7bce2c2b590f40"
   license "Apache-2.0"
 
-  depends_on "node"
+  depends_on "node@16"
 
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    # We have to replace the shebang in the main executable from "/usr/bin/env node" to point to the keg-only node@16
+    inreplace "#{libexec}/lib/node_modules/apify-cli/src/bin/run", "/usr/bin/env node", "#{Formula["node@16"].opt_bin}/node"
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
